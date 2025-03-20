a96e92fcd071e0379d856427a57420ff7cfcf217
fanfinfon <134154271+fanfinfon@users.noreply.github.com>
2025-03-20 22:40:47 +0300

    Initial commit

---
+++ database/seeders/YourTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class YourTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('about_pages')->insert([
+          [
+            'title' => 'Value 1',
+            'body' => 'Value 2',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+          [
+            'title' => 'Value 1',
+            'body' => 'Value 2',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+          [
+            'title' => 'Value 1',
+            'body' => 'Value 2',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+        ]);
+
+        DB::table('books')->insert([
+          [
+            'title' => 'Value 1',
+            'author' => 'Value 2',
+            'description' => 'Value 3',
+            'image' => 'Value 4',
+            'link' => 'Value 5',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+          [
+            'title' => 'Value 1',
+            'author' => 'Value 2',
+            'description' => 'Value 3',
+            'image' => 'Value 4',
+            'link' => 'Value 5',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+          [
+            'title' => 'Value 1',
+            'author' => 'Value 2',
+            'description' => 'Value 3',
+            'image' => 'Value 4',
+            'link' => 'Value 5',
+            'created_at' => now(),
+            'updated_at' => now(),
+          ],
+        ]);
+    }
+}
