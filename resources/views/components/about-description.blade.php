4f706a7e9862e08070bc84a0adaa573a81468aea
fanfinfon <134154271+fanfinfon@users.noreply.github.com>
2025-03-23 12:06:14 +0300

    about text changed

--- resources/views/components/about-description.blade.php
+++ resources/views/components/about-description.blade.php
@@ -1,11 +1,8 @@
-<div class="mt-24 mb-16">
-    <h2 class="text-center text-2xl font-light text-gray-800 mb-6 tracking-wider">{{$title}}</h2>
-    <div class="w-12 h-px bg-gray-300 mx-auto mb-8"></div>
 
-    <div class="max-w-3xl mx-auto">
-        <p class="text-gray-600 leading-relaxed mb-6">
-            {{$body}}
-        </p>
-
-    </div>
+<div class="text-center mb-16">
+    <h1 class="text-2xl md:text-4xl font-light text-gray-800 mb-4 tracking-wider">{{$title}}</h1>
+    <div class="w-16 h-px bg-gray-300 mx-auto mb-6"></div>
+    <p class="max-w-2xl mx-auto text-gray-600 leading-relaxed">
+        {{$body}}
+    </p>
 </div>
